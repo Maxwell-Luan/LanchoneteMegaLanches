3e346819ad9700f21183191f0ee0270638668cf8
Hyde-R <hydewinter04@gmail.com>
2024-08-31 23:17:18 -0300

    Adicionando a classe Produto ao projeto.

---
+++ src/entities/Produto.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Produto {
+
+	private int id;
+	private String descricao;
+	private double valor;
+	
+	public Produto(int id, String descricao, double valor) {
+		this.id = id;
+		this.descricao = descricao;
+		this.valor = valor;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+	
+	
+}
--- src/entities/Usuario.java
+++ src/entities/Usuario.java
@@ -8,100 +8,116 @@ import conexaobd.UsuarioBancoOperacoes;
 
 public class Usuario {
 
-	private String nome;
-	private String email;
-	private String senha;
-	private UsuarioBancoOperacoes userBD = null;
+    private int id;
+    private String nome;
+    private String email;
+    private String senha;
+    private UsuarioBancoOperacoes userBD = null;
 
-	ArrayList<Usuario> usuarios = new ArrayList<>();
+    ArrayList<Usuario> usuarios = new ArrayList<>();
 
-	public Usuario() {
-		this.userBD = new UsuarioBancoOperacoes();
-	}
+    public Usuario() {
+        this.userBD = new UsuarioBancoOperacoes();
+    }
 
-	public Usuario(String nome, String email, String senha) {
-		this.nome = nome;
-		this.email = email;
-		this.senha = senha;
-	}
+    public Usuario(int id, String nome, String email, String senha) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.userBD = new UsuarioBancoOperacoes();
+    }
 
-	public String getNome() {
-		return nome;
-	}
+    public Usuario(String nome, String email, String senha) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.userBD = new UsuarioBancoOperacoes();
+    }
 
-	public void setNome(String nome) {
-		this.nome = nome;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public String getEmail() {
-		return email;
-	}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
+    public String getNome() {
+        return nome;
+    }
 
-	public String getSenha() {
-		return senha;
-	}
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 
-	public void setSenha(String senha) {
-		this.senha = senha;
-	}
-	
-	// Permitir login do usuário no sistema.
-		public boolean logarUsuario() throws SQLException {
-			String nome = JOptionPane.showInputDialog(null, "Digite seu nome");
-			String senha = JOptionPane.showInputDialog(null, "Digite sua senha");
+    public String getEmail() {
+        return email;
+    }
 
-			boolean encontradoBanco = userBD.validarLoginUsuarioBanco(nome, senha);
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-			if (encontradoBanco) {
-				JOptionPane.showMessageDialog(null, "Bem vindo a Mega Lanches!");
-				return true;
-			} else {
-				JOptionPane.showMessageDialog(null, "Acesso negado! Usuário ou senha inválidos.");
-				return false;
-			}
+    public String getSenha() {
+        return senha;
+    }
 
-		}
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    // Permitir login do usuário no sistema.
+    public boolean logarUsuario() throws SQLException {
+        String nome = JOptionPane.showInputDialog(null, "Digite seu nome");
+        String senha = JOptionPane.showInputDialog(null, "Digite sua senha");
 
-	// Realizar o cadastro de um usuario.
-	public void cadastrarUsuario() throws SQLException {
-		String nome = JOptionPane.showInputDialog(null, "Digite seu nome");
-		while (nome.length() < 1 || nome.isEmpty() || nome.isBlank()) {
-			nome = JOptionPane.showInputDialog(null, "Nome inválido! Digite novamente");
-		}
-		String email = JOptionPane.showInputDialog(null, "Digite seu e-mail");
-		while (email.length() < 1 || email.isEmpty() || email.isBlank()) {
-			email = JOptionPane.showInputDialog(null, "E-mail inválido! Digite novamente");
-		}
-		String senha = JOptionPane.showInputDialog(null, "Digite sua senha");
-		while (senha.length() < 1 || senha.isEmpty() || senha.isBlank()) {
-			senha = JOptionPane.showInputDialog(null, "Senha inválida! Digite novamente");
-		}
+        boolean encontradoBanco = userBD.validarLoginUsuarioBanco(nome, senha);
 
-		if (userBD.validarUsuarioBancoNome(nome) == true) {
-			JOptionPane.showMessageDialog(null,
-					"O nome digitado já foi escolhido por outro usuário. Por favor, defina um novo.");
-		} else if (userBD.validarUsuarioBancoEmail(email) == true) {
-			JOptionPane.showMessageDialog(null, "O email digitado já está em uso. Por favor, selecione outro email.");
-		} else {
-			userBD.inserirUsuarioBanco(nome, email, senha);
-			JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
-		}
-	}
-	
-	public void alterarUsuario() throws HeadlessException, SQLException {
-		String nome = JOptionPane.showInputDialog(null, "Qual usuário deseja alterar o nome?");
-		if (userBD.validarUsuarioBancoNome(nome) == false) {
-			JOptionPane.showMessageDialog(null,
-					"Usuário não encontrado!");
-		}
-		else {
-			String novoNome = JOptionPane.showInputDialog(null, "Altere o nome: ");
-			userBD.alterarUsuarioBanco(nome, novoNome);
-		}
-	}
+        if (encontradoBanco) {
+            JOptionPane.showMessageDialog(null, "Bem-vindo a Mega Lanches!");
+            return true;
+        } else {
+            JOptionPane.showMessageDialog(null, "Acesso negado! Usuário ou senha inválidos.");
+            return false;
+        }
+    }
+
+    // Realizar o cadastro de um usuario.
+    public void cadastrarUsuario() throws SQLException {
+        String nome = JOptionPane.showInputDialog(null, "Digite seu nome");
+        while (nome.length() < 1 || nome.isEmpty() || nome.isBlank()) {
+            nome = JOptionPane.showInputDialog(null, "Nome inválido! Digite novamente");
+        }
+        String email = JOptionPane.showInputDialog(null, "Digite seu e-mail");
+        while (email.length() < 1 || email.isEmpty() || email.isBlank()) {
+            email = JOptionPane.showInputDialog(null, "E-mail inválido! Digite novamente");
+        }
+        String senha = JOptionPane.showInputDialog(null, "Digite sua senha");
+        while (senha.length() < 1 || senha.isEmpty() || senha.isBlank()) {
+            senha = JOptionPane.showInputDialog(null, "Senha inválida! Digite novamente");
+        }
+
+        if (userBD.validarUsuarioBancoNome(nome) == true) {
+            JOptionPane.showMessageDialog(null,
+                    "O nome digitado já foi escolhido por outro usuário. Por favor, defina um novo.");
+        } else if (userBD.validarUsuarioBancoEmail(email) == true) {
+            JOptionPane.showMessageDialog(null, "O email digitado já está em uso. Por favor, selecione outro email.");
+        } else {
+            userBD.inserirUsuarioBanco(nome, email, senha);
+            JOptionPane.showMessageDialog(null, "Usuário cadastrado com sucesso!");
+        }
+    }
+    
+    public void alterarUsuario() throws HeadlessException, SQLException {
+        String nome = JOptionPane.showInputDialog(null, "Qual usuário deseja alterar o nome?");
+        if (userBD.validarUsuarioBancoNome(nome) == false) {
+            JOptionPane.showMessageDialog(null,
+                    "Usuário não encontrado!");
+        } else {
+            String novoNome = JOptionPane.showInputDialog(null, "Altere o nome: ");
+            userBD.alterarUsuarioBanco(nome, novoNome);
+        }
+    }
 
 }
